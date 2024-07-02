7780231f97790111684c73155295cc28ab624003
Stefan Bilharz <stefan@sbsoftware.de>
2024-07-02 22:12:03 +0200

    CRM-5 Fix MenuController

--- src/menu_controller.cr
+++ src/menu_controller.cr
@@ -1,7 +1,8 @@
 class Crumble::Material::MenuController < Stimulus::Controller
   targets :menu
 
-  action "switch" do
+  action "switch" do |event|
+    event.preventDefault._call
     this.menuTarget.classList.toggle(Classes::MenuActive.to_js_ref)
   end
 end
