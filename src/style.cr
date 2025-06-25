8de6630166c15c18ff5b2dbf45065260053a86b6
Stefan Bilharz <stefan@sbsoftware.de>
2025-06-25 20:59:59 +0200

    CRM-16 Extend default style

--- src/style.cr
+++ src/style.cr
@@ -25,12 +25,38 @@ class Crumble::Material::Style < CSS::Stylesheet
       minHeight 100.vh
       padding 0
       margin 0
-      fontFamily "Roboto, sans-serif"
     end
 
     rule body do
       display Flex
       alignItems Stretch
+    end
+
+    rule body, button, input do
+      fontFamily "Roboto, sans-serif"
+    end
+
+    rule a do
+      color Black
+      textDecoration None
+    end
+
+    rule "[data-action]:not(input)" do
+      prop("cursor", "pointer")
+    end
+
+    rule "input[type=\"text\"]", "input[type=\"number\"]" do
+      prop("border", "none")
+      prop("border-bottom", "1px solid black")
+      backgroundColor "#EEE"
+      padding 8.px
+      prop("box-sizing", "border-box")
+    end
+
+    rule button do
+      prop("border", "none")
+      prop("outset", "none")
+      backgroundColor "transparent"
     end
   end
 end
